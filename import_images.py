825ef72bca8b4c57df2d055e841940adf7fac66c
Lisa-ach <166977216+Lisa-ach@users.noreply.github.com>
2025-02-04 10:35:56 +0100

    Create import_images.py

---
+++ import_images.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+import cv2
+import numpy as np
+
+def load_images_cv2(folder_path):
+    images = []
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
+            img_path = os.path.join(folder_path, filename)
+            img = cv2.imread(img_path)  # Charge l'image en format BGR (par défaut)
+            if img is not None:
+                images.append(img)
+    return images
+
+
+images_normal = load_images_cv2("potholes/normal/")
+images_potholes = load_images_cv2("potholes/potholes/")
+
+images = images_normal + images_potholes
+Y = [0]*len(images_normal) +[1]*len(images_potholes)
