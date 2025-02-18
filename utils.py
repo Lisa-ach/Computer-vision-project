0145867404080401b46316efb68009d26be1ac91
Lisa-ach <166977216+Lisa-ach@users.noreply.github.com>
2025-02-18 17:38:09 +0100

    Removing best_processing function that was redondant and modifying return perform_classification

--- utils.py
+++ utils.py
@@ -70,41 +70,8 @@ def perform_classification(method_name, df_Y,
     metrics_results, predictions, models = env.TrainTest()
     env.evaluate_model(models[best_method_name])
 
-    # Store and return Metrics
-    return env.get_metrics(models[best_method_name], method_name, metrics_results_best_methods), df_features
-
-def best_preprocessing(i, feature_extraction_method, filter_name, method_name, n_iter=10, fixed_histogram_method=None):
-    """
-    Find the best preprocessing configuration for a given feature extraction method.
-
-    :param i: Instance of ImagesProcessing to handle images.
-    :type i: ImagesProcessing
-
-    :param feature_extraction_method: Function to extract features from images.
-    :type feature_extraction_method: function
-
-    :param filter_name: Name of the preprocessing filter to apply.
-    :type filter_name: str
-
-    :param method_name: Name of the feature extraction method.
-    :type method_name: str
-
-    :param n_iter: Number of iterations to find the best preprocessing configuration.
-    :type n_iter: int
-
-    :return: Best preprocessing configuration and all results from the search.
-    :rtype: tuple (dict, list)
-
-    """
-
-    # Optimize preprocessing configuration
-    best_config, all_results = i.find_best_preprocessing(
-        feature_extraction_method=feature_extraction_method,  
-        associated_filter=filter_name,
-        n_iter=n_iter,
-        fixed_histogram_method=fixed_histogram_method
-    )
-
-    print(f"Best preprocessing configuration for {method_name}: {best_config}")
-    
-    return best_config, all_results
+    # If PCA we return also the number of components used
+    if dim_reduction == "PCA":
+        return env.get_metrics(models[best_method_name], method_name, metrics_results_best_methods), df_features, data.pca_n_components
+    else:
+        return env.get_metrics(models[best_method_name], method_name, metrics_results_best_methods), df_features
